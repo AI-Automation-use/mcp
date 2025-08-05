a9381dc14604d6a98a7e6c99858048ec4db28d5b
AI-Automation-use <guru.km@sonata-software.com>
2025-08-05 15:50:37 +0530

    Update server.py

--- server.py
+++ server.py
@@ -3,21 +3,31 @@ from mcp.server.fastmcp import FastMCP
 from mcp.server.streamable_http import StreamableHTTPServerTransport
 from tools.email_creator import register_email_tool
 from tools.onboarding_handler import register_onboarding_tool
-
-# Initialize MCP and transport globally
+from tools.profile_activatioon import register_profile_activation_tool
+from tools.employee_details import register_employee_tool
+from tools.update_emp_details import register_email_update_tool
+from tools.tax_changes import register_tax_news_tool
+from tools.email_drafter import register_draft_email_tool
+# from mcp.server.auth. import 
+# Initialize MCP and transport
 mcp = FastMCP("Employee Automation Server")
 register_email_tool(mcp)
 register_onboarding_tool(mcp)
-
+register_profile_activation_tool(mcp)
+register_employee_tool(mcp)
+register_email_update_tool(mcp)
+register_tax_news_tool(mcp)
+register_draft_email_tool(mcp)
 transport = StreamableHTTPServerTransport(
     mcp_session_id="employee-automation"
 )
 
-# Expose ASGI app via MCP's streamable HTTP method
+#ASGI app via MCP's streamable HTTP method
 app = mcp.streamable_http_app()
 
-# Setup MCP execution logic on startup
+#execution logic on startup
 @app.on_event("startup")
 async def startup():
     await transport.connect()
     asyncio.create_task(mcp.run_streamable_http_async())
+
